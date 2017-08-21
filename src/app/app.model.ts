ef6d3552a162ff1f73c86a38490eb596c7c614a9
Ramakrishna Melam <ram.melam@gmail.com>
2017-08-21 18:30:09 +1000

    Implemented Loader.

--- src/app/app.model.ts
+++ src/app/app.model.ts
@@ -1,32 +1,37 @@
-export interface CityModel {
-    City: string;
-    Country: string;
-}
-
-export interface Weather {
-    country: string;
-    location: string;
-    time: string;
-    wind: string;
-    visibility: string;
-    skyConditions: string;
-    temperature: string;
-    dewPoint: string;
-    relativeHumidity: string;
-    pressure: string;
-}
-
-export const defaultWeather: Weather = {
-    country: '',
-    location: '',
-    time: '',
-    wind: '',
-    visibility: '',
-    skyConditions: '',
-    temperature: '',
-    dewPoint: '',
-    relativeHumidity: '',
-    pressure: ''
-};
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { SelectModule } from 'angular2-select';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SearchComponent } from './components/Search/Search.component';
+import { SearchService } from './components/Search/Search.service';
+import { WeatherReportComponent } from './components/WeatherReport/WeatherReport.component';
+import { SelectCityComponent } from './components/SelectCity/SelectCity.component';
+import { LoadingModule } from 'ngx-loading';
 
 
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    SearchComponent,
+    SelectCityComponent,
+    WeatherReportComponent
+],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    ReactiveFormsModule,
+    FormsModule,
+    SelectModule,
+    LoadingModule
+  ],
+
+  providers: [SearchService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
